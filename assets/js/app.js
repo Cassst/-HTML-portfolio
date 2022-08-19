a607e9d9f8fd1530b6049fe4b96984d5cbf3710f
Augusto <augusto.94.castro@gmail.com>
2022-08-19 09:42:49 -0300

    dark theme work

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,23 @@ const menuElement = document.querySelector('.menu');
 
 const toggleMenu = () => {
     menuElement.classList.toggle('active');
+    menuToggleButton.classList.toggle('active');
 };
-
 menuToggleButton.addEventListener('click',toggleMenu);
 // !REMOVE ACTIVE CLASS FROM AND ICON ON LINK CLICK
-
+const removeActiveLinkClass = e =>{
+    if(e.target.classList.contains('list-link')){
+        menuElement.classList.remove('active');
+        menuToggleButton.classList.remove('active');
+    }
+}
+document.addEventListener('click',removeActiveLinkClass);
 // !TOGGLE THEME AND STORE SELECTION WITHIN LOCAL STORAGE
+const themeToggleButton = document.querySelector('.theme-toggle-button');
+const bodyElement = document.body;
 
+const toggleTheme = () =>{
+    bodyElement.classList.toggle('dark-theme');
+};
+themeToggleButton.addEventListener('click',toggleTheme);
 // !SCROLL REVEAL
