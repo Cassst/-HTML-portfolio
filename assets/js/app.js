5f06c92efe70fa276772c511ecc35e39c1a576f6
Augusto <augusto.94.castro@gmail.com>
2022-08-19 10:14:55 -0300

    changes update

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,9 +18,20 @@ document.addEventListener('click',removeActiveLinkClass);
 // !TOGGLE THEME AND STORE SELECTION WITHIN LOCAL STORAGE
 const themeToggleButton = document.querySelector('.theme-toggle-button');
 const bodyElement = document.body;
+const curretnTheme = localStorage.getItem('darkTheme');
+
+if(curretnTheme){
+    bodyElement.classList.add('dark-theme');
+}
 
 const toggleTheme = () =>{
     bodyElement.classList.toggle('dark-theme');
+
+    if(bodyElement.classList.contains('dark-theme')){
+        localStorage.setItem('dark-theme','active');
+    }else{
+        localStorage.removeItem('dark-theme');
+    }
 };
 themeToggleButton.addEventListener('click',toggleTheme);
 // !SCROLL REVEAL
